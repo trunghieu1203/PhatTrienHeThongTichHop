b88ce818e4aea0d313d26ad8eb5d38fe34e69072
trunghieu1203 <72431726+trunghieu1203@users.noreply.github.com>
2020-10-26 21:39:49 +0700

    Add files via upload

---
+++ Client_chat.java
@@ -0,0 +1,46 @@
+package lab4_chat;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client_chat {
+	private static Scanner inFromServer;
+	private static Scanner scan;
+
+	public static void main(String[] args) {
+		try {
+		@SuppressWarnings("resource")
+		Socket client = new Socket("LocalHost", 1241);
+		System.out.println("Client da duoc tao");
+		inFromServer = new Scanner(client.getInputStream());
+		PrintStream outToServer = new PrintStream(client.getOutputStream());
+		System.out.println("server: " + inFromServer.nextLine());
+		while(true) {
+			System.out.print(">>>");
+			scan = new Scanner(System.in);
+			String text = scan.nextLine();
+			outToServer.println(text);
+			String txt = inFromServer.nextLine();
+			System.out.println("server: " + txt);
+			if(text.equals("exit")) {
+				break;
+			}
+			if(txt.equals("exit")) {
+				System.out.println("server: " + inFromServer.nextLine());
+				break;
+			}		
+			}
+		
+		} catch (UnknownHostException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
+		}
+
+}
